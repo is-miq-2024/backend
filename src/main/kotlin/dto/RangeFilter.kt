90aad365c6d6ebe7e0125e6c369130a1f3e2f58a
Anna <snusha.spb@gmail.com>
2024-11-12 00:07:35 +0300

    added route filters

--- src/main/kotlin/dto/RangeFilter.kt
+++ src/main/kotlin/dto/RangeFilter.kt
@@ -3,11 +3,11 @@ package org.example.dto
 import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(name = "Number pattern", description = "The pattern for number values")
+@Schema(name = "RangeFilter", description = "Range filter for number values")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class RangeFilter<T>(
-    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, description = "Lower bound pattern")
+    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, description = "Lower bound")
     val lowerBound: T?,
-    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, description = "Upper bound pattern")
+    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, description = "Upper bound")
     val upperBound: T?
 )
