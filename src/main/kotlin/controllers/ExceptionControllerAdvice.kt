a954d5e6dbacf0f6a809cfc5e812d52186d23710
Anna <snusha.spb@gmail.com>
2024-11-12 00:16:26 +0300

    added route filters

--- src/main/kotlin/controllers/ExceptionControllerAdvice.kt
+++ src/main/kotlin/controllers/ExceptionControllerAdvice.kt
@@ -1,6 +1,5 @@
 package org.example.controllers
 
-import org.example.exception.ErrorMessage
 import org.example.exception.RouteException
 import org.example.exception.UserException
 import org.springframework.http.HttpStatus
@@ -12,13 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class ExceptionControllerAdvice {
 
     @ExceptionHandler(RouteException::class, UserException::class, IllegalArgumentException::class)
-    fun handleIllegalStateException(ex: RuntimeException): ResponseEntity<ErrorMessage> {
-
-        val errorMessage = ErrorMessage(
-            HttpStatus.NOT_FOUND.value(),
-            ex.message.orEmpty()
-        )
-
-        return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
+    fun handleIllegalStateException(ex: RuntimeException): ResponseEntity<String> {
+        return ResponseEntity(ex.message, HttpStatus.BAD_REQUEST)
     }
 }
