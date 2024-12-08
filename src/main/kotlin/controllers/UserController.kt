0b299abb839a92c5d3a17a529b42d9a32952dfc2
ulyagff <ulyagff@gmail.com>
2024-12-08 11:11:04 +0300

    add user func

--- src/main/kotlin/controllers/UserController.kt
+++ src/main/kotlin/controllers/UserController.kt
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.User
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -24,28 +25,28 @@ class UserController(
 ) {
 
     @GetMapping("/findByLogin")
-    fun findUserByLogin(@RequestParam login: String): ResponseEntity<UserResponse> {
+    fun findUserByLogin(@PathVariable login: String): ResponseEntity<UserResponse> {
         val user = userService.findByLogin(login)
         return ResponseEntity.ok(userMapper.userToDto(user))
     }
 
     @PostMapping("/addFavoriteRoute")
-    fun addFavoriteRoute(@RequestParam routeId: UUID) {
+    fun addFavoriteRoute(@PathVariable routeId: UUID) {
         userService.addFavoriteRoute(getCurrentUserLogin(), routeId)
     }
 
     @DeleteMapping("/deleteFavoriteRoute")
-    fun deleteFavoriteRoute(@RequestParam routeId: UUID) {
+    fun deleteFavoriteRoute(@PathVariable routeId: UUID) {
         userService.deleteFavoriteRoute(getCurrentUserLogin(), routeId)
     }
 
     @PostMapping("/addCompletedRoute")
-    fun addCompletedRoute(@RequestParam routeId: UUID) {
+    fun addCompletedRoute(@PathVariable routeId: UUID) {
         userService.addCompletedRoute(getCurrentUserLogin(), routeId)
     }
 
     @DeleteMapping("/deleteCompletedRoute")
-    fun deleteCompletedRoute(@RequestParam routeId: UUID) {
+    fun deleteCompletedRoute(@PathVariable routeId: UUID) {
         userService.deleteCompletedRoute(getCurrentUserLogin(), routeId)
     }
 
