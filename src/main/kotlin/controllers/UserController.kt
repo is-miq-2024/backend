9b44ba5250669849a06ecb52409d1d003dc399c1
Anna <snusha.spb@gmail.com>
2024-12-11 18:47:37 +0300

    added cors

--- src/main/kotlin/controllers/UserController.kt
+++ src/main/kotlin/controllers/UserController.kt
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.User
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
 class UserController(
     @Autowired private val userService: UserService,
